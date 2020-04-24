32fbb50285c53538ed0c8645d93be206a0e00194
wjyiwjyi <wjyi2015@163.com>
2020-04-24 11:44:44 +0800

    split Login and Register components and add Hot MicroNotice

--- mk-fe/src/components/Home.vue
+++ mk-fe/src/components/Home.vue
@@ -4,40 +4,24 @@
 			<v-head></v-head>
 		</el-header>
 		<el-main>
-			<div style="height: 360px">
-				<el-image class="image" :src="url" :fit="fit"></el-image>
-				<el-tabs v-model="activeName" style="display: inline-block; margin-left: 15%; height: 300px">
-					<el-tab-pane label="登录" name="first">
-						<el-form :model="dataForm" :rules="rules">
-							<el-form-item label="邮箱" prop="email">
-								<el-input type="text" v-model="dataForm.email" placeholder="请输入账号"></el-input>
-							</el-form-item>
-							<el-form-item label="密码" prop="password">
-								<el-input type="password" v-model="dataForm.password" placeholder="请输入密码"></el-input>
-							</el-form-item>
-							<el-button @click="login">登录</el-button>
-						</el-form>
-					</el-tab-pane>
-					<el-tab-pane label="注册" name="second">
-						<el-form :model="dataForm" :rules="rules">
-							<el-form-item label="昵称" prop="name">
-								<el-input type="text" v-model="dataForm.name" placeholder="请输入昵称"></el-input>
-							</el-form-item>
-							<el-form-item label="邮箱" prop="email">
-								<el-input type="text" v-model="dataForm.email" placeholder="请输入邮箱"></el-input>
-							</el-form-item>
-							<el-form-item label="密码" prop="password">
-								<el-input type="password" v-model="dataForm.password" placeholder="请输入密码"></el-input>
-							</el-form-item>
-							<el-button @click="register">注册</el-button>
-							<div class="remind">将对您的邮箱进行验证</div>
-						</el-form>
-					</el-tab-pane>
-				</el-tabs>
-			</div>
-			<div>
-
-			</div>
+		  <div style="height: 360px">
+        <el-image class="image" :src="url" :fit="fit"></el-image>
+        <el-tabs v-model="activeName" style="display: inline-block; margin-left: 15%; height: 300px">
+          <el-tab-pane label="登录" name="first">
+            <v-login></v-login>
+          </el-tab-pane>
+          <el-tab-pane label="注册" name="second">
+            <v-register></v-register>
+          </el-tab-pane>
+        </el-tabs>
+      </div>
+      <el-card style="width: 800px; margin: auto">
+        <div slot="header" class="cardTitle">热榜</div>
+        <div class="text item">
+          <v-notice></v-notice>
+          <v-notice></v-notice>
+        </div>
+      </el-card>
 		</el-main>
 		<v-footer></v-footer>
 	</el-container>
@@ -46,68 +30,32 @@
 <script>
 import vHead from './common/Header.vue';
 import vFooter from './common/Footer.vue';
+import vNotice from './common/notice.vue';
+import vLogin from './Login.vue';
+import vRegister from './Register.vue';
   export default {
     name: "Home",
     data() {
       return {
         fit: 'fill',
         activeName: 'first',
-        dataForm: {name: '', password: '', email: ''},
         url: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
-        rules: {
-          username: [{required: true, message: '账号不可为空', trigger: 'blur'}],
-          password: [{required: true, message: '密码不可为空', trigger: 'blur'}],
-          email: [{required: true, message: '邮箱不可为空', trigger: 'blur'}],
-        }
+        activeName2: 'first',
+        type: "微猜想",
+        title: "震惊！冯如杯要写不完了？！",
+        author: "uc主编",
+        keyWordTags: ['machine learning', 'c++从入门到入土'],
+        main_text: "冯如杯写不完是怎么回事呢？冯如杯相信大家都很熟悉，但是冯如杯写不完是怎么回事呢，下面就让小编带大家一起了解吧。冯如杯写不完， 其实就是冯如杯就是憨憨， 大家可能会很惊讶冯如杯怎么会写不完呢？ 但事实就是这样， 小编也感到非常惊讶。这就是关于冯如杯写不完的事情了， 大家有什么想法呢， 欢迎在评论区告诉小编一起讨论哦！ 啦啦啦啦啦啦啦",
       }
     },
 		components: {
 			vHead,
-			vFooter
+			vFooter,
+			vNotice,
+			vLogin,
+			vRegister
 		},
 		methods: {
-      login () {
-        var params = new URLSearchParams();
-        params.append('email', this.dataForm.email);
-        params.append('password',this.dataForm.password);
-				this.axios.post('/user/login', params)
-          .then((res)=>{
-            var remindType = res.data.code == 0 ? 'success' : 'info';
-            var remindTitle = res.data.code == 0 ? '登陆成功' : '登陆失败';
-            var remindContent = res.data.code == 0 ? '欢迎来到微知！' : '好像哪里出了问题/(ㄒoㄒ)/~~再试一次吧';
-            if (res.data.code==0){
-              this.$router.push("/user");
-            }
-						this.$alert(remindContent, remindTitle, {
-              confirmButtonText: '确定',
-              callback: action => {
-                this.$message({
-                  type: remindType,
-                  message: remindTitle
-                });
-              }
-            });
-          })
-          .catch((res)=>{
-						this.loginFail = true;
-            console.log(res.message);
-          });
-      },
-			register () {
-        var params = new URLSearchParams();
-        params.append('name', this.dataForm.username);
-        params.append('email', this.dataForm.email);
-        params.append('password',this.dataForm.password);
-        this.axios.post('/user/register', params)
-          .then((res)=>{
-            if (res.data.code==0){
-              this.$router.push("/home");
-            }
-          })
-          .catch((res)=>{
-            console.log(res.data.message);
-          });
-      }
     }
 
   }
@@ -134,5 +82,15 @@ import vFooter from './common/Footer.vue';
     margin-left: 20%;
     vertical-align: top;
     padding-top: 32px;
+  },
+
+  .card {
+    width: 800px;
+    margin: auto;
+  }
+
+  .cardTitle {
+    font-size: 20px;
+    text-align: center;
   }
 </style>
