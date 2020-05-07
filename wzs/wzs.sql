b924ed3d71cde6aab758b6a29575efbb8632c893
hao <qishiqihao@gmail.com>
2020-05-07 17:58:12 +0800

    add comment to sql, del evidence and guess

--- wzs/wzs.sql
+++ wzs/wzs.sql
@@ -54,53 +54,53 @@ INSERT INTO `admin` VALUES (1, '123456', 'admin@163.com');
 -- ----------------------------
 -- Table structure for micro_evidence
 -- ----------------------------
-DROP TABLE IF EXISTS `micro_evidence`;
-CREATE TABLE `micro_evidence`  (
-   `id` int(0) NOT NULL AUTO_INCREMENT,
-  `authorId` int(0) NULL DEFAULT NULL,
-  `topic` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `citedPaper` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `keywords` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `summary` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-   `time` datetime(0) NULL DEFAULT NULL,
-  `judge` int(0) NULL DEFAULT 0,
-   `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-    PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+-- DROP TABLE IF EXISTS `micro_evidence`;
+-- CREATE TABLE `micro_evidence`  (
+--    `id` int(0) NOT NULL AUTO_INCREMENT,
+--   `authorId` int(0) NULL DEFAULT NULL,
+--   `topic` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+--   `citedPaper` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+--   `keywords` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+--   `summary` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+--    `time` datetime(0) NULL DEFAULT NULL,
+--   `judge` int(0) NULL DEFAULT 0,
+--    `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+--     PRIMARY KEY (`id`) USING BTREE
+-- ) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of micro_evidence
 -- ----------------------------
-INSERT INTO `micro_evidence` VALUES (3, 1, '1-2-13', '123-312-4', '1-2-3', 'title111111111', '2020-04-28 18:55:30', 0, 'title332');
-INSERT INTO `micro_evidence` VALUES (4, 0, '', '1232-213', 'undefined', '213123124141412', '2020-04-26 16:25:45', 0, '12312414');
-INSERT INTO `micro_evidence` VALUES (5, 0, '1-4', '124', '213213-123', '213123', '2020-04-26 17:25:04', 0, '21321');
-INSERT INTO `micro_evidence` VALUES (6, 0, '2', '123', '124', '123', '2020-04-26 17:29:31', 0, '123');
-INSERT INTO `micro_evidence` VALUES (7, 0, '13-14', '213', '43243', '213', '2020-04-26 17:29:50', 0, '123123');
-INSERT INTO `micro_evidence` VALUES (8, 0, '22', '231', '123', '1243', '2020-04-26 17:31:24', 0, '14');
-INSERT INTO `micro_evidence` VALUES (9, 0, '13', '14', '12321', '124', '2020-04-26 17:32:50', 0, '124');
-INSERT INTO `micro_evidence` VALUES (10, 0, '20-14', '122', '123-12321', '213123', '2020-04-27 14:22:18', 0, '123123');
+-- INSERT INTO `micro_evidence` VALUES (3, 1, '1-2-13', '123-312-4', '1-2-3', 'title111111111', '2020-04-28 18:55:30', 0, 'title332');
+-- INSERT INTO `micro_evidence` VALUES (4, 0, '', '1232-213', 'undefined', '213123124141412', '2020-04-26 16:25:45', 0, '12312414');
+-- INSERT INTO `micro_evidence` VALUES (5, 0, '1-4', '124', '213213-123', '213123', '2020-04-26 17:25:04', 0, '21321');
+-- INSERT INTO `micro_evidence` VALUES (6, 0, '2', '123', '124', '123', '2020-04-26 17:29:31', 0, '123');
+-- INSERT INTO `micro_evidence` VALUES (7, 0, '13-14', '213', '43243', '213', '2020-04-26 17:29:50', 0, '123123');
+-- INSERT INTO `micro_evidence` VALUES (8, 0, '22', '231', '123', '1243', '2020-04-26 17:31:24', 0, '14');
+-- INSERT INTO `micro_evidence` VALUES (9, 0, '13', '14', '12321', '124', '2020-04-26 17:32:50', 0, '124');
+-- INSERT INTO `micro_evidence` VALUES (10, 0, '20-14', '122', '123-12321', '213123', '2020-04-27 14:22:18', 0, '123123');
 
 -- ----------------------------
 -- Table structure for micro_guess
 -- ----------------------------
-DROP TABLE IF EXISTS `micro_guess`;
-CREATE TABLE `micro_guess`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT,
-  `authorId` int(0) NULL DEFAULT NULL,
-  `topic` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci  NULL DEFAULT NULL,
-  `citedEvidList` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci  NULL DEFAULT NULL,
-  `keywords` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci  NULL DEFAULT NULL,
-  `summary` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci  NULL DEFAULT NULL,
-  `time` datetime(0) NULL DEFAULT NULL,
-  `judge` int(0) NULL DEFAULT 0,
-  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci  NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE utf8_general_ci  ROW_FORMAT = Dynamic;
+-- DROP TABLE IF EXISTS `micro_guess`;
+-- CREATE TABLE `micro_guess`  (
+--   `id` int(0) NOT NULL AUTO_INCREMENT,
+--   `authorId` int(0) NULL DEFAULT NULL,
+--   `topic` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci  NULL DEFAULT NULL,
+--   `citedEvidList` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci  NULL DEFAULT NULL,
+--   `keywords` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci  NULL DEFAULT NULL,
+--   `summary` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci  NULL DEFAULT NULL,
+--   `time` datetime(0) NULL DEFAULT NULL,
+--   `judge` int(0) NULL DEFAULT 0,
+--   `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci  NULL DEFAULT NULL,
+--   PRIMARY KEY (`id`) USING BTREE
+-- ) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE utf8_general_ci  ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of micro_guess
 -- ----------------------------
-INSERT INTO `micro_guess` VALUES (3, 0, '1-13-4', '4-9-6', '13231-12', 'test1231221', '2020-04-29 00:08:49', 0, 'test11');
+-- INSERT INTO `micro_guess` VALUES (3, 0, '1-13-4', '4-9-6', '13231-12', 'test1231221', '2020-04-29 00:08:49', 0, 'test11');
 
 -- ----------------------------
 -- Table structure for micro_notice
@@ -227,5 +227,53 @@ CREATE TABLE `user_info`  (
 INSERT INTO `user_info` VALUES (1, 'wzs01@163.com', '张华', '', 'male', '天津市', 'fighting!', '博士', '研究员', '1-2', '1-2-3', '对数据挖掘领域感兴趣', '研究方向为数据挖掘');
 INSERT INTO `user_info` VALUES (2, 'wzs02@163.com', '王小伟', '', 'male', '北京市', 'work hard', '本科生', '学生', '1-2', '1-2-3', '对数据挖掘领域感兴趣', '研究方向为数据挖掘');
 INSERT INTO `user_info` VALUES (3, 'wzs03@163.com', '王莞尔', '', 'female', '浙江省杭州市', 'work hard', '本科生', '学生', '1-2', '1-2-3', '对数据挖掘领域感兴趣', '研究方向为数据挖掘');
+
+-- ----------------------------
+-- Table structure for comment
+-- ----------------------------
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE `comment`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `noticeId` int(0) NULL DEFAULT NULL,
+  `authorId` int(0) NULL DEFAULT NULL,
+  `fromId` int(0) NULL DEFAULT NULL,
+  `fromName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `toId` int(0) NULL DEFAULT NULL,
+  `toName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `content` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 28 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of comment
+-- ----------------------------
+INSERT INTO `comment` VALUES (1, 1, 1, 2, 'Tom', 0, NULL, 'abcdef', '2020-05-01 15:37:41');
+INSERT INTO `comment` VALUES (2, 2, 2, 3, 'Jerry', 0, NULL, '132435465789', '2020-05-01 15:37:41');
+INSERT INTO `comment` VALUES (3, 1, 1, 3, 'Jerry', 2, 'Tom', 'ljhjo123143', '2020-05-01 15:37:41');
+INSERT INTO `comment` VALUES (4, 1, 1, 2, 'Tom', 0, NULL, 'abcdef', '2020-05-01 15:37:40');
+INSERT INTO `comment` VALUES (5, 2, 2, 3, 'Jerry', 0, NULL, '132435465789', '2020-05-01 15:37:44');
+INSERT INTO `comment` VALUES (6, 1, 1, 3, 'Jerry', 2, 'Tom', 'ljhjo123143', '2020-05-01 15:37:44');
+INSERT INTO `comment` VALUES (7, 1, 1, 0, 'from', -1, '', '评论评论', '2020-05-06 16:08:03');
+INSERT INTO `comment` VALUES (8, 1, 1, 0, 'from', 2, 'Tom', '1234567890', '2020-05-06 16:10:51');
+INSERT INTO `comment` VALUES (9, 1, 1, 0, 'from', -1, '', 'nbnbnb', '2020-05-06 20:52:40');
+INSERT INTO `comment` VALUES (10, 1, 1, 0, 'from', -1, '', '测试noticeId', '2020-05-06 20:53:53');
+INSERT INTO `comment` VALUES (11, 1, 1, 0, 'from', -1, '', 'test add 1', '2020-05-06 20:55:30');
+INSERT INTO `comment` VALUES (12, 1, 1, 0, 'from', -1, '', 'test add 2', '2020-05-06 20:55:51');
+INSERT INTO `comment` VALUES (13, 1, 1, 0, 'from', 3, 'Jerry', 'test reply 1', '2020-05-06 20:56:38');
+INSERT INTO `comment` VALUES (14, 1, 1, 0, 'from', 0, 'from', '0987654321', '2020-05-06 20:58:11');
+INSERT INTO `comment` VALUES (15, 1, 1, 0, 'from', -1, '', '同步测试', '2020-05-06 21:00:25');
+INSERT INTO `comment` VALUES (16, 1, 1, 0, 'from', 0, 'from', '回复评论 同步测试', '2020-05-06 21:00:41');
+INSERT INTO `comment` VALUES (17, 1, 1, 0, 'from', -1, '', 'test 11111', '2020-05-06 21:05:09');
+INSERT INTO `comment` VALUES (18, 1, 1, 0, 'from', 0, 'from', '回复评论的同步测试', '2020-05-06 21:11:56');
+INSERT INTO `comment` VALUES (19, 1, 1, 0, 'from', 0, 'from', '回复的回复的回复', '2020-05-06 21:19:27');
+INSERT INTO `comment` VALUES (20, 1, 1, 0, 'from', 0, 'from', '回复的回复的回复的回复', '2020-05-06 21:21:31');
+INSERT INTO `comment` VALUES (21, 1, 1, 0, 'from', 0, 'from', '回复的回复的回复的回复的回复', '2020-05-06 21:22:42');
+INSERT INTO `comment` VALUES (22, 1, 1, 0, 'from', 0, 'from', '回复的回复的回复的回复的回复的回复', '2020-05-06 22:15:34');
+INSERT INTO `comment` VALUES (23, 1, 1, 0, 'from', 0, 'from', '回复的回复的回复的回复的回复的回复的回复！！！', '2020-05-06 22:16:53');
+INSERT INTO `comment` VALUES (24, 1, 1, 0, 'from', -1, '', '评论评论评论', '2020-05-06 22:17:03');
+INSERT INTO `comment` VALUES (25, 1, 1, 0, 'from', -1, '', 'test count', '2020-05-06 22:37:27');
+INSERT INTO `comment` VALUES (26, 1, 1, 0, 'from', -1, '', 'count change', '2020-05-06 22:38:54');
+INSERT INTO `comment` VALUES (27, 1, 1, 0, 'from', 0, 'from', 'reply count change', '2020-05-06 22:39:07');
 
 SET FOREIGN_KEY_CHECKS = 1;
