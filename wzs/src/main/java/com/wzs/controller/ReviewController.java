9dfb8602be8ca01ef52f7244e5858c7900fe4dd4
hao <qishiqihao@gmail.com>
2020-05-11 11:35:57 +0800

    remove old service

--- wzs/src/main/java/com/wzs/controller/ReviewController.java
+++ wzs/src/main/java/com/wzs/controller/ReviewController.java
@@ -1,7 +1,7 @@
 package com.wzs.controller;
 
-import com.wzs.service.MEvidService;
-import com.wzs.service.MGuessService;
+//import com.wzs.service.MEvidService;
+//import com.wzs.service.MGuessService;
 import com.wzs.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,11 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/review")
 public class ReviewController {
 
-    @Autowired
-    private MGuessService guessService;
-
-    @Autowired
-    private MEvidService mEvidService;
+//    @Autowired
+//    private MGuessService guessService;
+//
+//    @Autowired
+//    private MEvidService mEvidService;
 
     @Autowired
     private ReviewService reviewService;
