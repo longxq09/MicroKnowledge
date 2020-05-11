89ca84f27b51f48512ecb1f4f6515b372af27694
hao <qishiqihao@gmail.com>
2020-05-11 11:07:20 +0800

    添加点赞sql操作

--- wzs/wzs.sql
+++ wzs/wzs.sql
@@ -197,5 +197,25 @@ INSERT INTO `review` VALUES (3, 1, 0, 0, 0, '计算机科学与技术', 'citatio
 INSERT INTO `review` VALUES (3, 1, 1, 0, 0, '计算机科学与技术', 'citation', 'A,B,C,D', 'summary', '2020-04-21 02:58:00', 'title');
 INSERT INTO `review` VALUES (3, 1, 1, 0, 0, '计算机科学与技术', 'citation', 'A,B,C,D', 'summary', '2020-04-21 02:58:00', 'title');
 
+DROP TABLE IF EXISTS `like`;
+CREATE TABLE `like`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `noticeId` int(0) NULL DEFAULT NULL,
+  `authorId` int(0) NULL DEFAULT NULL,
+  `fromId` int(0) NULL DEFAULT NULL,
+  `fromName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `toId` int(0) NULL DEFAULT NULL,
+  `toName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `content` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 28 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of comment
+-- ----------------------------
+INSERT INTO `comment` VALUES (1, 1, 1, 2, 'Tom', 0, NULL, 'abcdef', '2020-05-01 15:37:41');
+
+
 SET FOREIGN_KEY_CHECKS = 1;
 
