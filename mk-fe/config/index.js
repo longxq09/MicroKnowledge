38ed48a54049952487c6df574f1b25f19601bf5a
hao <qishiqihao@gmail.com>
2020-06-05 15:25:30 +0800

    for merge

--- mk-fe/config/index.js
+++ mk-fe/config/index.js
@@ -18,6 +18,7 @@ module.exports = {
 		},
 
     // Various Dev Server settings
+    // host: 'localhost', // can be overwritten by process.env.HOST
     host: '114.115.151.39', // can be overwritten by process.env.HOST
     port: 8282, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
