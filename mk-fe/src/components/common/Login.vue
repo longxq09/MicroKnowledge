47321231f512c36d20d048405080875674163291
wjyiwjyi <wjyi2015@163.com>
2020-06-08 22:25:04 +0800

    delete forget password

--- mk-fe/src/components/common/Login.vue
+++ mk-fe/src/components/common/Login.vue
@@ -7,30 +7,6 @@
 			<el-input type="password" v-model="dataForm.password" placeholder="请输入密码"></el-input>
 		</el-form-item>
 		<el-button @click="login">登录</el-button>
-    <el-button type="text" @click="forgetPassVisible = true">忘记密码？</el-button>
-    <el-dialog title="找回密码" :visible.sync="forgetPassVisible" :append-to-body="true">
-      <el-steps :active="active" finish-status="success">
-        <el-step title="输入邮箱"></el-step>
-        <el-step title="输入邮箱验证码"></el-step>
-        <el-step title="重置密码"></el-step>
-      </el-steps>
-      <el-form :model="forgetForm" :rules="forgetRules" autocomplete="off">
-        <el-form-item v-if="active==0" label="邮箱" prop="email">
-          <el-input style="width: 300px" type="text" v-model="forgetForm.email"></el-input>
-        </el-form-item>
-        <el-form-item v-if="active==1" label="验证码" prop="code">
-          <el-input style="width: 300px" type="text" v-model="forgetForm.code"></el-input>
-        </el-form-item>
-        <el-form-item v-if="active==2" label="新密码" prop="password">
-          <el-input style="width: 300px" type="password" v-model="forgetForm.password"></el-input>
-        </el-form-item>
-        <el-form-item v-if="active==2" label="确认密码" prop="repeat">
-          <el-input style="width: 300px" type="password" v-model="forgetForm.repeat"></el-input>
-        </el-form-item>
-      </el-form>
-      <el-button style="margin-top: 12px;" @click="next" v-if="active<2">下一步</el-button>
-      <el-button style="margin-top: 12px" @click="submit" v-if="active==2">提交</el-button>
-    </el-dialog>
 	</el-form>
 </template>
 
@@ -78,52 +54,6 @@
       }
     },
     methods: {
-      next() {
-        if (this.active == 0) {
-          this.checkEmail()
-        } else {
-          this.checkCode()
-        }
-      },
-      checkEmail() {
-        var params = new URLSearchParams();
-        params.append('email', this.forgetForm.email);
-        this.axios.post('/user/forgetPassword', params)
-          .then((res)=>{
-            if (res.data.code == 0) {
-              this.active++
-            } else {
-              let remindTitle = '邮箱不存在！'
-              let remindContent = '请检查邮箱是否输入正确！'
-              this.$alert(remindContent, remindTitle, {
-                confirmButtonText: '确定'
-              });
-            }
-          })
-          .catch((res)=>{
-            console.log(res.message);
-          });
-      },
-      checkCode() {
-        var params = new URLSearchParams();
-        params.append('email', this.forgetForm.email);
-        params.append('code', this.forgetForm.code);
-        this.axios.post('/user/checkForgetCode', params)
-          .then((res)=>{
-            if (res.data.code == 0) {
-              this.active++
-            } else {
-              let remindTitle = '验证码错误！'
-              let remindContent = '请检查验证码是否输入正确！'
-              this.$alert(remindContent, remindTitle, {
-                confirmButtonText: '确定'
-              });
-            }
-          })
-          .catch((res)=>{
-            console.log(res.message);
-          });
-      },
       submit() {
         if (this.forgetForm.password.length < 6) {
           let remindTitle = '密码长度过短！'
@@ -194,29 +124,12 @@
             } else {
               sessionStorage.setItem("accountId", res.data.id)
               sessionStorage.setItem("email", this.dataForm.email)
-              this.$confirm('检测到您是管理员，是否进入管理页面', '界面选择', {
-                distinguishCancelAndClose: true,
-                confirmButtonText: '是，进入管理界面',
-                cancelButtonText: '否，进入用户界面'
-              })
-                .then(() => {
-                  this.$router.push('/admin/message')
-                  const h = this.$createElement;
-                  this.$notify({
-                    title: '登陆成功',
-                    message: h('i', { style: 'color: teal'}, '欢迎来到微知管理界面！')
-                  });
-                })
-                .catch(action => {
-                  if (action === 'cancel') {
-                    this.$router.push("/homepage");
-                    const h = this.$createElement;
-                    this.$notify({
-                      title: '登陆成功',
-                      message: h('i', { style: 'color: teal'}, '欢迎来到微知！')
-                    });
-                  }
-                });
+              this.$router.push('/admin/message')
+              const h = this.$createElement;
+              this.$notify({
+                title: '登陆成功',
+                message: h('i', { style: 'color: teal'}, '欢迎来到微知管理界面！')
+              });
             }
           })
           .catch((res)=>{
